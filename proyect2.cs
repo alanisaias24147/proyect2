4ecf1804cac1073a413a41a1c202e8c9bd1710bb
alanisaias24147 <78932267+alanisaias24147@users.noreply.github.com>
2021-04-22 07:05:47 +0200

    Add files via upload

---
+++ proyect2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace proyecto2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese el nombre que desea buscar");
+
+        }
+
+    }
+}
+     
+    
